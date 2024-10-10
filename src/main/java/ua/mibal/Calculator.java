748b4a7bed2c4557e47e3542159c67cb1ea7e036
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-10 18:20:01 +0200

    Init `Launcher`

--- src/main/java/ua/mibal/Calculator.java
+++ src/main/java/ua/mibal/Calculator.java
@@ -4,8 +4,9 @@ package ua.mibal;
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Calculator {
+
+    public void run() {
+        
     }
 }
