5c95ad0c068da74326e2f29d36f38bdeb0f699de
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-10 18:28:26 +0200

    Implement `Expression`

--- src/main/java/ua/mibal/model/Operation.java
+++ src/main/java/ua/mibal/model/Operation.java
@@ -4,9 +4,9 @@ package ua.mibal.model;
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-public record Expression(
-        int left,
-        Operation operation,
-        int right
-) {
+public enum Operation {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
 }
