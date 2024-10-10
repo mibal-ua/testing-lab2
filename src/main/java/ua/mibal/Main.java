6df62ddfceb0aa5e1f74ce635b131a7852922f01
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-10 18:12:05 +0200

    Init version

---
+++ src/main/java/ua/mibal/Main.java
@@ -0,0 +1,11 @@
+package ua.mibal;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
